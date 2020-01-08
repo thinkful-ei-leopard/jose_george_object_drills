f05c17bd7686bcf61e81ed231461545d0f9e79fa
Maghion <geo.f.medina@gmail.com>
2020-01-07 23:32:30 -0500

    added decoder function

--- app.js
+++ app.js
@@ -52,66 +52,105 @@
 //   return newArr
 // }
 
-function enrollInSummerSchool(students) {
-  // your code here
+// function enrollInSummerSchool(students) {
+//   // your code here
 
-  for (let i =0; i <students.length; i++){
-    students[i].status = 'In Summer School';
-  }
-  return students;
-}
+//   for (let i =0; i <students.length; i++){
+//     students[i].status = 'In Summer School';
+//   }
+//   return students;
+// }
 
-function findById(items, idNum) {
-  for(let i = 0; i < items.length; i++) {
-    if(items[i].id === idNum) {
-      return items[i]
-    }
-  }
-}
+// function findById(items, idNum) {
+//   for(let i = 0; i < items.length; i++) {
+//     if(items[i].id === idNum) {
+//       return items[i]
+//     }
+//   }
+// }
 
-function validateKeys(object, expectedKeys) {
-  let newArr = Object.keys(object);
+// function validateKeys(object, expectedKeys) {
+//   let newArr = Object.keys(object);
 
-  for(let i = 0; i < expectedKeys.length; i++) { 
-    for(let n = 0; n < newArr.length; i++) { 
-      if(expectedKeys.length === newArr.length && expectedKeys[i] === newArr[n]) { 
-        return true } else { 
-        return false } } }
+//   for(let i = 0; i < expectedKeys.length; i++) { 
+//     for(let n = 0; n < newArr.length; i++) { 
+//       if(expectedKeys.length === newArr.length && expectedKeys[i] === newArr[n]) { 
+//         return true } else { 
+//         return false } } }
 
-}
+// }
 
-const loaf = {
-  flour: 300,
-  water: 210,
-  hydration() {
-    return (this.water / this.flour) * 100
-  }
-};
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration() {
+//     return (this.water / this.flour) * 100
+//   }
+// };
 
 // console.log(loaf.flour)
 
 // console.log(loaf.hydration())
 
-const bob = {
-  foo: 1,
-  bar: 2,
-  fum: 3,
-  quux: 4,
-  spam: 5,
+// const bob = {
+//   foo: 1,
+//   bar: 2,
+//   fum: 3,
+//   quux: 4,
+//   spam: 5,
+// }
+// for (let key in bob) {
+//   console.log(`${key} ${bob[key]}`)
+// }
+
+// const mcds = {
+//   meals: ['breakfast', 'second breakfast',
+//     'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+// };
+
+// console.log(mcds.meals[3]);
+
+// const jobs = [{name: 'Chandler', jobTitle: 'Accountant', boss: 'steve'}, {name: 'Phoebe', jobTitle: 'masseuss', boss: 'jose'}, {name: 'Joey', jobTitle: 'Actor', boss: 'will'}, {name: 'Monica', jobTitle: 'Chef'}];
+// // jobs.forEach(element => console.log(`${element.name} ${element.jobTitle}`));
+
+// jobs.forEach(element => !element.boss ? console.log(`${element.jobTitle} ${element.name} doesn't report to anybody`) : console.log(`${element.jobTitle} ${element.name} reports to ${element.boss}.`));
+
+function decode(word) {
+  const cipher = {
+    a: 2,
+    b: 3,
+    c: 4,
+    d: 5,
+  };
+  const newArr = word.split(' ');
+  let decoded = [];
+  console.log(newArr);
+  for (let i = 0; i < newArr.length; i++){
+    if (Object.keys(cipher).indexOf(newArr[i][0]) !== -1) {
+      decoded.push(newArr[i][cipher[newArr[i][0]]-1])     
+    }
+    else {
+      decoded.push(' ');
+    }
+  }
+ console.log(decoded.join(''));
+
 }
-for (let key in bob) {
-  console.log(`${key} ${bob[key]}`)
-}
+decode('craft block argon meter bells brown croon droop');
 
-const mcds = {
-  meals: ['breakfast', 'second breakfast',
-    'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
-};
+// function decode2(word) {
+//   if (word[0].toLowerCase() === 'a') {
+//     return word[1];
+//   } else if (word[0].toLowerCase() === 'b') {
+//     return word[2];
+//   } else if (word[0].toLowerCase() === 'c') {
+//     return word[3];
+//   } else if (word[0].toLowerCase() === 'd') {
+//     return word[4];
+//   } else {
+//     return ' ';
+//   }
+// }
 
-console.log(mcds.meals[3]);
 
-const jobs = [{name: 'Chandler', jobTitle: 'Accountant', boss: 'steve'}, {name: 'Phoebe', jobTitle: 'masseuss', boss: 'jose'}, {name: 'Joey', jobTitle: 'Actor', boss: 'will'}, {name: 'Monica', jobTitle: 'Chef'}];
-// jobs.forEach(element => console.log(`${element.name} ${element.jobTitle}`));
-
-jobs.forEach(element => !element.boss ? console.log(`${element.jobTitle} ${element.name} doesn't report to anybody`) : console.log(`${element.jobTitle} ${element.name} reports to ${element.boss}.`));
 
